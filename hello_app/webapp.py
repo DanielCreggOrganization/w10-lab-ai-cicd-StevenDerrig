977411de18e7301933a46eb37aebc076802adbee
Steebo <127858400+StevenDerrig@users.noreply.github.com>
2025-11-26 19:27:01 +0000

    Simulating a vulnerability with a hard coded API key

--- hello_app/webapp.py
+++ hello_app/webapp.py
@@ -2,5 +2,8 @@
 from . import app    # For application discovery by the 'flask' command. 
 from . import views  # For import side-effects of setting up routes. 
 
+# In hello_app/webapp.py
+AWS_SECRET_KEY = "AKIA1234567890"
+
 # Time-saver: output a URL to the VS Code terminal so you can easily Ctrl+click to open a browser
 # print('http://127.0.0.1:5000/hello/VSCode')
