4c1c3fd48622c69f7e16d4406bea92f7c84c2bdc
Steebo <127858400+StevenDerrig@users.noreply.github.com>
2025-11-27 09:08:11 +0000

    Add feature with debug logs

--- hello_app/webapp.py
+++ hello_app/webapp.py
@@ -7,3 +7,10 @@ AWS_SECRET_KEY = "AKIA1234567890"
 
 # Time-saver: output a URL to the VS Code terminal so you can easily Ctrl+click to open a browser
 # print('http://127.0.0.1:5000/hello/VSCode')
+
+# Adding a debug print that shouldn't be in production
+def potentially_slow_function():
+    print("DEBUG: Starting function...") 
+    import time
+    time.sleep(1) # Hardcoded delay
+    return True
